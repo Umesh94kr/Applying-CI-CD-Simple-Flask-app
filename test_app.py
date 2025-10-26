15749b164c9c29fd2abc17d3c772f01d97e52c39
Umesh94kr <umeshkumar94628@gmail.com>
2025-10-26 23:01:23 +0530

    CI-CD Added

--- test_app.py
+++ test_app.py
@@ -1,7 +1,8 @@
 from app import app 
 
 def test_app():
-    response = app.test_client.get('/')
-
+    client = app.test_client()   # <-- parentheses here
+    response = client.get('/')   # now you can call .get()
+    
     assert response.status_code == 200
-    assert response.data == b'Hello World!'
+    assert b'Hello World!' in response.data
